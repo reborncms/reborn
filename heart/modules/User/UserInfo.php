4e0479ec73bb7705a321780bc1cbc68035a486ee
khay <khayusaki@gmail.com>
2013-08-08 15:12:58 +0800

    Update Module Info in User and Theme.

--- heart/modules/User/UserInfo.php
+++ heart/modules/User/UserInfo.php
@@ -6,7 +6,17 @@ class UserInfo extends \Reborn\Module\AbstractInfo
 {
 	protected $name = 'User';
 
+	protected $displayName = array(
+		'en' => 'User', 
+		'my' => 'အသုံးပြုသူများ'
+	);
+
 	protected $version = '1.0';
+
+	protected $description = array(
+		'en' => 'User, Group, and Permission managament with Cataclyst\Sentry.', 
+		'my' => 'အသုံးပြုသူများ၊ အဖွဲ့နေရာစီမံ ခန့်ခွဲခြင်း နှင့် မော်ဂျူး ခွင့်ပြုချက်များအား လုပ်ဆောင်နိုင်ပါသည်။ Cataclyst\Sentry ကို အသုံးပြုထားသည်။'
+	);
 
 	protected $description = 'User, Group, and Permission managament with Cataclyst\Sentry.';
 
