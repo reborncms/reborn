17e315fcbf852295b355ea8b2c9e72d5be91b33a
DragonVirus <limonster.li@gmail.com>
2013-08-08 11:15:49 +0630

    mm language file added
    - Blog
    - Pages
    - Tag

---
+++ heart/modules/Pages/lang/my/pages.php
@@ -0,0 +1,49 @@
+<?php
+// Language file for page module
+
+return array(
+    'titles'                    => array(
+        'main_title'            => 'စာမျက်နှာများ',
+        'add_page'              => 'စာမျက်နှာ အသစ်ထည့်မည်။',
+        'add_page_info'         => 'စာမျက်နှာ အသစ်ထည့်မည်။',
+    ),
+    'page_contents'             => 'စာမျက်နှာတွင် ပါဝင်မည့် အရာများ',
+    'meta_data'                 => 'Meta Data',
+    'labels'                    => array(
+        'page_title'            => 'စာမျက်နှာ ခေါင်းစဉ်',
+        'page_slug'             => 'Page Slug',
+        'status'                => 'Status',
+        'page_settings'         => 'စာမျက်နှာ အပြင်အဆင်များ',
+        'page_layout'           => 'စာမျက်နှာ ပုံစံ',
+        'page_contents'         => 'စာမျက်နှာတွင် ပါဝင်မည့် အရာများ',
+        'comment_status'        => 'မှတ်ချက် ပေးခွင့် အခြေအနေ',
+        'comment_enabled'       => 'Comments Enabled',
+        'page_meta_data'        => 'Page Meta Data',
+        'meta_title'            => 'Meta Title',
+        'meta_keywords'         => 'Meta Keywords',
+        'meta_description'      => 'Meta Description',
+        'design'	            => 'ဒီဇိုင်း',
+        'page_duplicate'        => 'ဒီစာမျက်နှာကို ပွားမည်။',
+    ),
+    'messages'                  => array(
+        'success'               => array(
+            'add'               => 'စာမျက်နှာ အသစ် အောင်မြင်စွာ ပြုလုပ်ပြီးပါပြီ။',
+            'edit'              => 'စာမျက်နှာ ကို အောင်မြင်စွာပြင်ဆင်ပြီးပါပြီ။',
+            'delete'            => 'စာမျက်နှာ ကို အောင်မြင်စွာ ဖျက် ပြီးပါပြီ။',
+            'status_update'     =>  'စာမျက်နှာ အခြေအနေ အောင်မြင်စွာ ပြောင်းလဲပြီးသည်။',
+            'autosave_on'       => 'စာမျက်နှာ အား %s တွင် အလိုအလျောက် သိမ်းထားသည်။',
+        ),
+        'error'                 => array(
+            'add'               => 'စာမျက်နှာ အသစ်ပြုလုပ်ရာတွင် အမှားအယွင်း တစ်ခုခုဖြစ်နေပါသည်။',
+            'edit'              => 'စာမျက်နှာ ပြင်ဆင်ရာတွင် အမှားအယွင်း တစ်ခုခုဖြစ်နေပါသည်။',
+            'delete'            => 'စာမျက်နှာ ဖျက်ရာတွင် အမှားအယွင်း တစ်ခုခုဖြစ်နေပါသည်။',
+            'slug_duplicate'    => '*** ဒီနာမည်နှင့် slug ရှိပြီးသားဖြစ်နေပါသည်။ တခြားနာမည်တစ်ခုပြန်ရွေးစေလိုပါတယ်။',
+            'status_update'     => 'စာမျက်နှာ အခြေအနေ ပြောင်းလဲရာတွင် အမှားအယွင်း တစ်ခုခုဖြစ်နေသည်။',
+            'csrf_error'        => 'This is probably a CSRF attack.',
+            'not_found_head'    => '404 Not Found!!!',
+            'not_found_body'    => 'What are you looking for ?',
+        ),
+    ),
+);
+
+// end of pages.php
---
+++ heart/modules/Tag/lang/my/tag.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Langauge File for Tag Module
+ */
+
+return array(
+	'title_main'			=> 'Tag',
+	'title_create'			=> 'Tag အသစ်ပြုလုပ်မည်',
+	'title_edit'			=> 'Tag အားပြင်ဆင်မည်',
+
+	/* // Table Label
+	'post_on'				=> 'Posted On',
+	
+	// Frontend Label
+	'posted_on'				=> 'Posted on',
+	'posted_by'				=> 'Posted by',
+	'tag_by'				=> 'Tagged',
+	
+	//Breadcrumb
+	'b_blog'				=> 'Blog',
+	'b_category'			=> 'Category',
+	'b_author'				=> 'Author - ',
+	'b_archive'				=> 'Archive for %s',
+	'b_tag'					=> 'Tag - ', */
+
+	// Form Label
+	'title'					=> 'Tag နာမည်',
+	'slug'					=> 'Slug',
+
+	// Error Message
+	'csrf_error'			=> 'Error occur for security reason!',
+	'create_error'			=> 'Tag အသစ် %s ပြုလုပ်ရာတွင် အမှားအယွင်း တစ်ခုခုဖြစ်နေပါသည်။',
+	'edit_error'			=> 'Tag %s အားပြင်ဆင်ရာတွင် အမှားအယွင်း တစ်ခုခုဖြစ်နေပါသည်။',
+	'delete_error'			=> 'Tag %s အားဖျက်ရာတွင် အမှားအယွင်း တစ်ခုခုဖြစ်နေပါသည်။',
+	'already_exit'			=> 'Tag နာမည် %s သည်ရှိပြီးသားဖြစ်နေပါသည်။',
+
+	// Success Message
+	'create_success'		=> 'Tag အသစ် %s အောင်မြင်စွာ ပြုလုပ်ပြီးပါပြီ။',
+	'edit_success'			=> 'Tag %s ကို အောင်မြင်စွာပြင်ဆင်ပြီးပါပြီ။',
+	'delete_success'		=> 'Tag %s ကို အောင်မြင်စွာ ဖျက် ပြီးပါပြီ။',
+	'delete_success_many'	=> '%s Tagများ ကို အောင်မြင်စွာ ဖျက် ပြီးပါပြီ။',
+
+	// No Tag List in Index View
+	'no_tag_list'			=> 'အခုလောလောဆယ် Tag တစ်ခုမှမရှိသေးပါ။',
+	
+);
+
+
+/* End of tag.php */
