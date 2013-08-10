0cc261aacdfa00d083cc53b6c1146893119d52a7
nyanlynnhtut <lynnhtut87@gmail.com>
2013-08-10 22:14:46 +0630

    Update Admin Panel Theme Info.

--- heart/themes/default/info.php
+++ heart/themes/default/info.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-		'name' => 'Stupid Reborn Admin Theme',
+		'name' => 'Reborn Admin Default Theme',
 		'description' => 'Flat Design Admin theme for the Reborn CMS',
 		'author' => 'Reborn Development Team',
 		'website' => 'http://www.reborncms.com',
