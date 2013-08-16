174a505c3be48aa3c41e1c4505455a72ce2fa862
nyanlynnhtut <lynnhtut87@gmail.com>
2013-08-16 13:20:18 +0630

    Update process for CSRF

--- heart/reborn/src/Reborn/Util/Security.php
+++ heart/reborn/src/Reborn/Util/Security.php
@@ -17,20 +17,30 @@ class Security
 {
 
     /**
-     * CSRF key name to get from config
-     *
-     * @var string
-     **/
-    protected static $csrf_key;
-
-    /**
-     * Class Init
+     * Make CSRF Token
      *
      * @return void
      **/
-    public function __construct()
+    public static function makeCSRFToken($key = null)
     {
-        static::$csrf_key = Config::get('app.security.csrf_key');
+        $csrf_key = static::getKey($key);
+
+        $token = static::getCSRFToken($csrf_key);
+
+        $session = static::getSession();
+
+        $session->set($csrf_key, $token);
+    }
+
+    /**
+     * Refresh the CSRF Token
+     *
+     * @param string $key CSRF-Key
+     * @return void
+     **/
+    public static function refreshToken($key = null)
+    {
+        static::makeCSRFToken($key);
     }
 
     /**
@@ -40,51 +50,13 @@ class Security
      * @param boolean $refresh Make refresh the token key
      * @return string
      **/
-    public static function CSRField($key = null, $refresh = false)
+    public static function CSRField($key = null)
     {
-        if (is_null($key)) {
-            $csrf_key = Config::get('app.security.csrf_key');
-        } else {
-            $csrf_key = $key.'_'.Config::get('app.security.csrf_key');
-        }
-        $token =  self::CSRFtoken($key, $refresh);
+        $csrf_key = static::getKey($key);
+
+        $token =  self::getCSRFToken($csrf_key);
+
         return Form::hidden($csrf_key, $token);
-    }
-
-    /**
-     * Return CSFR Key Only
-     *
-     * @param string $key
-     * @param boolean $refresh Make refresh the token key
-     * @return string
-     **/
-    public static function CSRFKeyOnly($key = null, $refresh = false)
-    {
-        return static::CSRFtoken($key, $refresh);
-    }
-
-    /**
-     * Generate CSRFToken
-     *
-     * @param string $key
-     * @param boolean $refresh Make refresh the token key
-     * @return string
-     **/
-    protected static function CSRFtoken($key = null, $refresh = false)
-    {
-        if (is_null($key)) {
-            $csrf_key = Config::get('app.security.csrf_key');
-        } else {
-            $csrf_key = $key.'_'.Config::get('app.security.csrf_key');
-        }
-
-        $session = Registry::get('app')->session;
-
-        if ($refresh) {
-            static::refreshToken($session, $csrf_key);
-        }
-
-        return $session->get($csrf_key);
     }
 
     /**
@@ -94,14 +66,11 @@ class Security
      **/
     public static function CSRFvalid($key = null)
     {
-        $session = Registry::get('app')->session;
         $request = Registry::get('app')->request;
 
-        if (is_null($key)) {
-            $csrf_key = Config::get('app.security.csrf_key');
-        } else {
-            $csrf_key = $key.'_'.Config::get('app.security.csrf_key');
-        }
+        $session = static::getSession();
+
+        $csrf_key = static::getKey($key);
 
         if ($request->isAjax() && \Input::isPost()) {
             $ckey = $request->headers->get('X-CSRF-Token');
@@ -109,23 +78,60 @@ class Security
             $ckey = \Input::get($csrf_key);
         }
 
-        if ($session->has($csrf_key) && $session->get($csrf_key) == $ckey) {
-            return true;
+        if ($session->has($csrf_key) && ($session->get($csrf_key) == $ckey) ) {
+            $result = true;
         } else {
-            return false;
+            $result = false;
         }
+
+        return $result;
     }
 
     /**
-     * Refresh the CSRF Token
+     * Get CSRF Token
      *
-     * @param Symfony\Component\HttpFoundation\Session\Session $session
-     * @param string $key CSRF-Key
-     * @return void
+     * @param string $key
+     * @return string
      **/
-    protected static function refreshToken($session, $key)
+    protected static function getCSRFToken($key)
     {
-        $session->remove($key);
+        $session = static::getSession();
+
+        if ($session->has($key)) {
+            $token = $session->get($key);
+        } else {
+            $token = static::getHaskToken();
+        }
+
+        return $token;
+    }
+
+    /**
+     * Get CSRF Key Name
+     *
+     * @param string $key CSRF Key Prefix
+     * @return string
+     **/
+    protected static function getKey($key = null)
+    {
+        // Set csrf_key
+        if (is_null($key)) {
+            $csrf_key = Config::get('app.security.csrf_key');
+        } else {
+            $csrf_key = $key.'_'.Config::get('app.security.csrf_key');
+        }
+
+        return $csrf_key;
+    }
+
+    /**
+     * Get Hashing Value for CSRF Token
+     *
+     * @return string
+     **/
+    protected static function getHaskToken()
+    {
+        //Make CSRF Token
         $encypt_method = Config::get('app.security.token_encrypt');
 
         if ( ! in_array($encypt_method, array('md5', 'sha1', 'random')) ) {
@@ -140,6 +146,16 @@ class Security
             $token = $encypt_method(uniqid(rand(), true));
         }
 
-        $session->set($key,$token);
+        return $token;
+    }
+
+    /**
+     * Get Session Object
+     *
+     * @return Symfony\Component\HttpFoundation\Session\Session
+     **/
+    protected static function getSession()
+    {
+        return Registry::get('app')->session;
     }
 }
