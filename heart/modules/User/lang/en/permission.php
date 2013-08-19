073564ca769932ecca279a5fe1c0612ce027727d
khay <khayusaki@gmail.com>
2013-08-19 15:10:45 +0800

    User/Theme bug fixes

--- heart/modules/User/lang/en/permission.php
+++ heart/modules/User/lang/en/permission.php
@@ -2,6 +2,7 @@
 
 return array(
 		'title' 	=> 'Module Permissions',
+		'info'		=> 'Set permission for this usergroup. If you set empty check for permission, usergroup will not have access permission for follow module.',
 		'menu'		=> 'Permissions',
 		'admin' 	=> 'User Admin',
 		'groupName'	=> 'Group Name',
