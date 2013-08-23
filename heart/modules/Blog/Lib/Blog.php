8934b949f7c9d92a04900a24221dc5917137b3ec
nyanlynnhtut <lynnhtut87@gmail.com>
2013-08-23 23:10:19 +0630

    Update Blog::post() for ViewData

--- heart/modules/Blog/Lib/Blog.php
+++ heart/modules/Blog/Lib/Blog.php
@@ -15,6 +15,7 @@ class Blog
 		$offset = isset($options['offset']) ? (int)$options['offset'] : null;
 		$order = isset($options['order']) ? $options['order'] : 'created_at';
 		$dir = isset($options['order_dir']) ? $options['order_dir'] : 'desc';
+		$template = isset($options['template']) ? $options['template'] : 'default';
 
 		$posts = Model::active();
 		if (!is_null($cats)) {
@@ -24,6 +25,6 @@ class Blog
 
 		$posts = $posts->orderBy($order, $dir)->take($limit)->skip($offset)->get();
 
-		return with($ins = new PostPresenter($posts))->posts();
+		return with($ins = new PostPresenter($posts))->posts($template);
 	}
 }
