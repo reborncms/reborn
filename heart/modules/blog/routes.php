a617943a8a7c36ce345a508758b84cd10eb4e38d
DragonVirus <limonster.li@gmail.com>
2013-09-18 11:40:38 +0630

    Unicode char support in slugs
    - Blog
    - Page
    - Blog Category
    - Tag

--- heart/modules/blog/routes.php
+++ heart/modules/blog/routes.php
@@ -3,6 +3,14 @@
 // Route file for module Blog
 \Route::add('rss_feed', 'blog/rss', 'Blog\Blog::rss');
 
-\Route::add('blog_archives', 'blog/archives', 'Blog\Blog::archives');
+\Route::add('blog_archives', 'blog/archives/{:int}/{:?}', 'Blog\Blog::archives');
 
-\Route::add('blog_single', 'blog/{:alnum}', 'Blog\Blog::view');
+\Route::add('blog_preview', 'blog/preview/{:any}', 'Blog\Blog::preview');
+
+\Route::add('blog_category', 'blog/category/{:any}/{:?}', 'Blog\Blog::category');
+
+\Route::add('blog_tag', 'blog/tag/{:any}/{:?}', 'Blog\Blog::tag');
+
+\Route::add('blog_author', 'blog/author/{:any}/{:?}', 'Blog\Blog::author');
+
+\Route::add('blog_single', 'blog/{:any}', 'Blog\Blog::view');
