370875550c27ebfe4b89bd442f47f6b712c45327
Mr-Zer0 <bulletson.geek@gmail.com>
2013-10-07 16:47:10 +0630

    Adding route for thumbnail.

--- heart/modules/media/routes.php
+++ heart/modules/media/routes.php
@@ -1,4 +1,9 @@
 <?php
 
+	// Viewing images
 	\Route::get('media/image/{:target}/{int:width}?/{int:height}?',
 		'Media\Media::image', 'image_preview_with_name');
+
+	// Setting thumbnail
+	\Route::get('admin/media/thumbnail/{int:folderId}/{alpha:target}?',
+		'Media\Admin\Media::thumbnail', 'thumbnail');
