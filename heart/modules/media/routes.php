26d03b024cfda68edf08a4851de8ef18218f52a9
Mr-Zer0 <bulletson.geek@gmail.com>
2013-10-07 16:30:58 +0630

    Fix "No file options".

--- heart/modules/media/routes.php
+++ heart/modules/media/routes.php
@@ -1,10 +1,4 @@
 <?php
-	$adminUrl = \Config::get('app.adminpanel');
 
-	Route::add('editFile', $adminUrl.'/media/edit/file/{:int}', 
-		'Media\Admin\Media::editFile');
-
-	Route::add('editFolder', $adminUrl.'/media/edit/folder/{:int}',
-		'Media\Admin\Media::editFolder');
-
-	Route::add('seeFile', '/file/{:alnum}', 'Media\Media::image');
+	\Route::get('media/image/{:target}/{int:width}?/{int:height}?',
+		'Media\Media::image', 'image_preview_with_name');
