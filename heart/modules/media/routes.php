164e89c168f808f0a4fc148fe96b0d7291c2fac8
Mr-Zer0 <bulletson.geek@gmail.com>
2013-10-13 13:15:42 +0630

    Adding routes for functions : - Folder delete - File delete - File upload - Folder create

--- heart/modules/media/routes.php
+++ heart/modules/media/routes.php
@@ -7,3 +7,18 @@
 	// Setting thumbnail
 	\Route::get('admin/media/thumbnail/{int:folderId}/{alpha:target}?',
 		'Media\Admin\Media::thumbnail', 'thumbnail');
+
+	// File upload
+	\Route::post('admin/media/upload/{int:folderid}?/{string:key}?',
+		'Media\Admin\Media::upload', 'file_upload');
+
+	// Folder Delete
+	\Route::delete('admin/media/delete-folder/{int:id}',
+		'Media\Admin\Media::deleteFolder', 'folder_delete');
+
+	// File Delete
+	\Route::delete('admin/media/delete-file/{int:id}',
+		'Media\Admin\Media::deleteFile', 'file_delete');
+
+	\Route::post('admin/media/create-folder/{int:folderId}?',
+		'Media\Admin\Media::createFolder', 'folder_create');
