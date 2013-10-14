4af689b46601a535bc5d3b09bccc5b5dfdfd0d8a
Mr-Zer0 <bulletson.geek@gmail.com>
2013-10-14 22:54:57 +0630

    Added remaining routes.

--- heart/modules/media/routes.php
+++ heart/modules/media/routes.php
@@ -5,20 +5,29 @@
 		'Media\Media::image', 'image_preview_with_name');
 
 	// Setting thumbnail
-	\Route::get('admin/media/thumbnail/{int:folderId}/{alpha:target}?',
+	\Route::get('@admin/media/thumbnail/{int:folderId}/{alpha:target}?',
 		'Media\Admin\Media::thumbnail', 'thumbnail');
 
 	// File upload
-	\Route::post('admin/media/upload/{int:folderid}?/{string:key}?',
+	\Route::post('@admin/media/upload/{int:folderid}?/{string:key}?',
 		'Media\Admin\Media::upload', 'file_upload');
 
 	// Folder Delete
-	\Route::delete('admin/media/delete-folder/{int:id}',
+	\Route::add('@admin/media/delete-folder/{int:id}',
 		'Media\Admin\Media::deleteFolder', 'folder_delete');
 
 	// File Delete
-	\Route::delete('admin/media/delete-file/{int:id}',
+	\Route::add('@admin/media/delete-file/{int:id}/{int:redirect}?',
 		'Media\Admin\Media::deleteFile', 'file_delete');
 
-	\Route::post('admin/media/create-folder/{int:folderId}?',
+	\Route::post('@admin/media/create-folder/{int:folderId}?',
 		'Media\Admin\Media::createFolder', 'folder_create');
+
+	\Route::get('@admin/media/explore/{int:id}', 'Media\Admin\Media::explore',
+		'explorer');
+
+	\Route::add('@admin/media/edit-folder/{int:id}',
+		'Media\Admin\Media::editFolder', 'edit_folder');
+
+	\Route::add('@admin/media/edit-file/{int:id}',
+		'Media\Admin\Media::editFile', 'edit_file');
