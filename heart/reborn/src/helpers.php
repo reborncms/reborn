3f774d3cc844019d358d3628c50835c7b4d17c07
nyanlynnhtut <lynnhtut87@gmail.com>
2014-01-09 12:30:34 +0630

    update ga() function for Google Analytics at helpers.php

--- heart/reborn/src/helpers.php
+++ heart/reborn/src/helpers.php
@@ -1034,19 +1034,28 @@ if (!function_exists('ga')) {
 	 * https://developers.google.com/analytics/devguides/collection/analyticsjs/
 	 *
 	 * @param string $gid Google analytics ID
+	 * @param string $domain Domain name of tracking site
 	 * @return string
 	 **/
-	function ga($gid)
+	function ga($gid, $domain = null)
 	{
+		if (is_null($domain)) {
+			$domain = url();
+		}
+
+		$domain = str_replace(array('http://', 'https://', 'www.'), '', $domain);
+
 		return <<<GA
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','$gid');ga('send','pageview');
-        </script>
+<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', '$gid', '$domain');
+		ga('send', 'pageview');
+
+	</script>
 GA;
 	}
 }
