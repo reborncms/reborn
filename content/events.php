19fd7bc6b545b2f03f5c84806059e8317d14daec
nyanlynnhtut <lynnhtut87@gmail.com>
2014-02-11 14:48:20 +0630

    Add event regsiter file at content folder

---
+++ content/events.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Event Register File for Developer
+|--------------------------------------------------------------------------
+|
+| You can register custom event on this file.
+| This file is nothing change from REBORN. So you can use without git conflict
+|
+*/
