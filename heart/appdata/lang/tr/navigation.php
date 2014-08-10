26f88f7bba24a2ec17a53e5af997a045f92533eb
Asil Balaban <mail@asilbalaban.com>
2014-08-10 21:24:38 +0300

    Turkish language support for app files

---
+++ heart/appdata/lang/tr/navigation.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Turkish for ReBorn CMS.
+ *
+ * @package    ReBorn CMS
+ * @version    2.0
+ * @author     Asil Balaban <mail@asilbalaban.com>
+ * @license    ReBorn CMS License
+ * @copyright  2012 ReBorn CMS Development Team
+ * @link       http://www.reborncms.com
+ */
+
+return array(
+
+	'dashboard'		=> 'Genel Bakış',
+	'content'		=> 'İçerikler',
+	'blog'			=> 'Blog',
+	'pages'			=> 'Sayfalar',
+	'comments'		=> 'Yorumlar',
+	'media'			=> 'Medya',
+	'media_manager'	=> 'Medya Yöneticisi',
+	'utilities'		=> 'Araçlar',
+	'appearance'	=> 'Görünüm',
+	'themes'		=> 'Temalar',
+	'users'			=> 'Kullanıcılar',
+	'user_management'=> 'Kullanıcı Yönetimi',
+	'permission'	=> 'İzinler',
+	'settings'		=> 'Ayarlar',
+	'modules'		=> 'Modüller',
+	'plugin'		=> 'Eklentiler',
+	'widget'		=> 'Araçlar'
+);
---
+++ heart/appdata/lang/tr/numbers.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Turkish for ReBorn CMS.
+ *
+ * @package    ReBorn CMS
+ * @version    2.0
+ * @author     Asil Balaban <mail@asilbalaban.com>
+ * @license    ReBorn CMS License
+ * @copyright  2012 ReBorn CMS Development Team
+ * @link       http://www.reborncms.com
+ */
+
+return array(
+
+		'formats' => array(
+				'0' => '0',
+				'1' => '1',
+				'2' => '2',
+				'3' => '3',
+				'4' => '4',
+				'5' => '5',
+				'6' => '6',
+				'7' => '7',
+				'8' => '8',
+				'9' => '9',
+				'am'=> 'öö',
+				'pm'=> 'ös',
+				'AM'=> 'ÖÖ',
+				'PM'=> 'ÖS',
+			)
+	);
