24fe9616bff66d5b89b2e85e1d9156e246592e09
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-01-16 01:02:09 +0630

    alpha to alnum at theme module route

--- heart/modules/theme/routes.php
+++ heart/modules/theme/routes.php
@@ -1,10 +1,10 @@
 <?php
 
 Route::get('@admin/theme/', 'Theme\Admin\Theme::index', 'theme_index');
-Route::get('@admin/theme/activate/{alpha:name}/', 'Theme\Admin\Theme::activate', 'theme_activate');
-Route::get('@admin/theme/delete/{alpha:name}/', 'Theme\Admin\Theme::delete', 'theme_delete');
+Route::get('@admin/theme/activate/{alnum:name}/', 'Theme\Admin\Theme::activate', 'theme_activate');
+Route::get('@admin/theme/delete/{alnum:name}/', 'Theme\Admin\Theme::delete', 'theme_delete');
 
 Route::add('@admin/theme/upload/', 'Theme\Admin\Theme::upload', 'theme_upload');
 
 Route::get('@admin/theme/editor/', 'Theme\Admin\Editor::index', 'theme_editor');
-Route::add('@admin/theme/editor/edit/{alpha:ext}/{alpha:file}', 'Theme\Admin\Editor::edit', 'editor_edit');
+Route::add('@admin/theme/editor/edit/{alnum:ext}/{alpha:file}', 'Theme\Admin\Editor::edit', 'editor_edit');
