a95b3409938703eb34ff547657c3f76ebd98526f
Nyan Lynn Htut <lynnhtut87@gmail.com>
2015-01-16 00:58:33 +0630

    Change theme activate route's rule

--- heart/modules/theme/routes.php
+++ heart/modules/theme/routes.php
@@ -3,6 +3,7 @@
 Route::get('@admin/theme/', 'Theme\Admin\Theme::index', 'theme_index');
 Route::get('@admin/theme/activate/{alpha:name}/', 'Theme\Admin\Theme::activate', 'theme_activate');
 Route::get('@admin/theme/delete/{alpha:name}/', 'Theme\Admin\Theme::delete', 'theme_delete');
+
 Route::add('@admin/theme/upload/', 'Theme\Admin\Theme::upload', 'theme_upload');
 
 Route::get('@admin/theme/editor/', 'Theme\Admin\Editor::index', 'theme_editor');
